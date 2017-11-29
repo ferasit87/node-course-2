534b440001ba88d605d8462c30cdb70d07d63870
Feras <ferasit87@gmail.com>
2017-11-29 11:18:03 +0300

    Add Post to doss and refactor mangoose

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} =  require('./models/todo.js');
+var {User} =  require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos' , (req, res) =>{
+  var todo = new Todo({
+   text: req.body.text
+   });
+  todo.save().then((doc)=>{
+    res.send(doc);
+    console.log("Saved", doc );
+  },(e) =>{
+      res.status(400).send(e);
+    console.log("Error saving",e);
+  })
+});
+
+
+
+app.listen(3000,()=>{
+  console.log("started on 3000");
+});
+// var newTodo = new Todo({
+// text: 'again  Edit this vedio '
+// });
+//
+// newTodo.save().then((doc)=>{
+//   console.log('Save ... ' , doc);
+// }, (err) => {
+//
+//   console.log("Error to save");
+//
+// });
